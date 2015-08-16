ce08f7c3ed99ea8fd358d16448e48ce62abde2f9
XiNGRZ <chenxingyu92@gmail.com>
2015-08-16 23:47:29 +0800

    支持旁注文字

--- editor.js
+++ editor.js
@@ -9,12 +9,19 @@ $(function () {
     var maxLength = 0
 
     var rendered = content.split('\n').map(function (row, i) {
-      var rows = row.replace(/~~.+$/, '').split('\\')
+      var rows = row.split('\\')
       if (rows.length > maxLength) {
         maxLength = rows.length
       }
-      return '<div>' + rows.map(function (cell, j) {
-        return '<div data-row="' + i + '" data-cell="' + j + '">' + cell.split('!~').map(function (layer) {
+      var text = ''
+      return '<div>' + rows.map(function (col, j) {
+        var divider = col.indexOf('~~')
+        if (divider >= 0) {
+          text = col.substring(divider).trim()
+          col = col.substring(0, divider)
+        }
+
+        return '<div class="cell" data-row="' + i + '" data-col="' + j + '" title="' + col + '">' + col.split('!~').map(function (layer) {
           layer = layer.trim()
           if (layer) {
             var name = 'BSicon_' + (layer.replace(/ /, '_')) + '.svg'
@@ -23,12 +30,12 @@ $(function () {
             return '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/' + file + '">'
           }
         }).join('') + '</div>'
-      }).join('') + '</div>'
+      }).join('') + '<div class="text" data-row="' + i + '" title="' + text + '">' + text + '</div></div>'
     }).join('')
 
     $preview.css('width', maxLength * 20).html(rendered)
 
-    var width = maxLength * 20 + 80
+    var width = maxLength * 20 + 80 + 200
     width = width < 200 ? 200 : width
     width = width > 600 ? 600 : width
 
@@ -39,34 +46,83 @@ $(function () {
   function syncFocus () {
     var selection = $editor.prop('selectionStart')
 
-    var rows = $editor.val().substring(0, selection).split('\n')
+    var content = $editor.val()
 
-    var row = rows.length - 1
-      , cell = rows.pop().split('\\').length - 1
+    var rows = content.substring(0, selection).split('\n')
+      , cols = rows.pop().split('\\')
+      , cell = cols.pop()
+      , text = cell + content.substring(selection).split('\n').shift()
+
+    var row = rows.length
+      , col = cols.length
 
     $preview.find('.focus').removeClass('focus')
-    $preview.find('> div > div[data-row="' + row + '"][data-cell="' + cell + '"]').addClass('focus')
+
+    var divider = text.indexOf('~~')
+      , heading = cell.length
+
+    if ((divider == 0 && heading >= 1) || (divider > 0 && heading > divider)) {
+      $preview.find('> div > .text[data-row="' + row + '"]').addClass('focus')
+    } else {
+      $preview.find('> div > .cell[data-row="' + row + '"][data-col="' + col + '"]').addClass('focus')
+    }
   }
 
-  $preview.on('click', '> div > div[data-row][data-cell]', function () {
+  $preview.on('click', '> div > .cell[data-row][data-col]', function () {
     var $this = $(this)
 
     var row = $this.data('row')
-      , cell = $this.data('cell')
+      , col = $this.data('col')
 
     var rows = $editor.val().split('\n').slice(0, row + 1)
-      , cells = rows.pop().split('\\').slice(0, cell + 1)
+      , cols = rows.pop().split('\\').slice(0, col + 1)
 
     var above = rows.join('\n').length
     above = rows.length ? above + 1 : 0
 
-    var selection = cells.pop()
+    var selection = cols.pop()
 
-    var cell = cells.join('\\').length
-    cell = cells.length ? cell + 1 : 0
+    var divider = selection.indexOf('~~')
+    if (divider >= 0) {
+      selection = selection.substring(0, divider)
+    }
 
-    var selectionStart = above + cell
+    var col = cols.join('\\').length
+    col = cols.length ? col + 1 : 0
+
+    var selectionStart = above + col
       , selectionEnd = selectionStart + selection.length
+
+    $editor
+      .prop('selectionStart', selectionStart)
+      .prop('selectionEnd', selectionEnd)
+      .focus()
+
+    $preview.find('.focus').removeClass('focus')
+    $this.addClass('focus')
+  })
+
+  $preview.on('click', '> div > .text[data-row]', function () {
+    var $this = $(this)
+
+    var row = $this.data('row')
+
+    var rows = $editor.val().split('\n').slice(0, row + 1)
+      , line = rows.pop()
+
+    var above = rows.join('\n').length
+    above = rows.length ? above + 1 : 0
+
+    var divider = line.indexOf('~~')
+
+    var selectionStart = above
+      , selectionEnd = selectionStart + line.length
+
+    if (divider > 0) {
+      selectionStart += divider
+    } else {
+      selectionStart += line.length
+    }
 
     $editor
       .prop('selectionStart', selectionStart)
