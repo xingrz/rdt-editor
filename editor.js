9455442e9679cad88f816938d5e0c7fa03038446
XiNGRZ <chenxingyu92@gmail.com>
2015-08-20 00:03:43 +0800

    以 Vue 重写渲染逻辑

--- editor.js
+++ editor.js
@@ -1,173 +1,207 @@
 $(function () {
-  var $preview = $('#preview')
-    , $editor = $('#editor')
 
-  var TEMPLATE_ROW = $('#template-row').html()
-    .replace(/\n/g, '')
-    .replace(/\>[ ]+\</g, '><')
-    .replace(/\>[ ]+\{\{/g, '>{{')
-    .replace(/\}\}[ ]+\</g, '}}<')
-    .trim()
-
-  Mustache.parse(TEMPLATE_ROW)
-
-  function render () {
-    var content = $editor.val().trim()
-
-    var maxLength = 0
-
-    var rendered = content.split('\n').map(function (row, i) {
-      maxLength = Math.max(maxLength, row.split('\\').length)
-      return renderRow(row, i)
-    }).join('')
-
-    $preview.css('width', maxLength * 20).html(rendered)
-
-    var width = Math.min(Math.max(maxLength * 20 + 80 + 200, 200), 600)
-
-    $('#preview-container').css('width', width)
-    $('#editor-container').css('right', width)
-  }
-
-  function renderRow (row, i) {
-    var structure = {
-      i: i,
-      cols: [],
-      text: ''
-    }
-
-    structure.cols = row.split('\\').map(function (col, j) {
-      var divider = col.indexOf('~~')
-      if (divider >= 0) {
-        structure.text = col.substring(divider).trim()
-        col = col.substring(0, divider)
-      }
-
-      var column = {
-        j: j,
-        raw: col,
-        layers: []
-      }
-
-      col.split('!~').forEach(function (layer) {
-        layer = layer.trim()
-        if (layer) {
-          var name = 'BSicon_' + (layer.replace(/ /, '_')) + '.svg'
-            , hash = CryptoJS.MD5(name).toString(CryptoJS.enc.Hex)
-          column.layers.push({
-            a: hash.substring(0, 1),
-            b: hash.substring(0, 2),
-            name: encodeURIComponent(name)
-          })
+  Vue.directive('selection', {
+    twoWay: true,
+    bind: function () {
+      this.handler = function () {
+        switch (this.arg) {
+          case 'start':
+            this.set(this.el.selectionStart)
+            break
+          case 'end':
+            this.set(this.el.selectionEnd)
+            break
         }
-      })
-
-      return column
-    })
-
-    return Mustache.render(TEMPLATE_ROW, structure)
-  }
-
-  function syncFocus () {
-    var selection = $editor.prop('selectionStart')
-
-    var content = $editor.val()
-
-    var rows = content.substring(0, selection).split('\n')
-      , cols = rows.pop().split('\\')
-      , cell = cols.pop()
-      , text = cell + content.substring(selection).split('\n').shift()
-
-    var row = rows.length
-      , col = cols.length
-
-    $preview.find('.focus').removeClass('focus')
-
-    var divider = text.indexOf('~~')
-      , heading = cell.length
-
-    if ((divider == 0 && heading >= 1) || (divider > 0 && heading > divider)) {
-      $preview.find('> div > .text[data-row="' + row + '"]').addClass('focus')
-    } else {
-      $preview.find('> div > .cell[data-row="' + row + '"][data-col="' + col + '"]').addClass('focus')
+      }.bind(this)
+      this.el.addEventListener('keyup', this.handler, false)
+      this.el.addEventListener('mouseup', this.handler, false)
+    },
+    update: function (value) {
+      switch (this.arg) {
+        case 'start':
+          this.el.selectionStart = value
+          break
+        case 'end':
+          this.el.selectionEnd = value
+          break
+      }
+    },
+    unbind: function () {
+      this.el.removeEventListener('keyup', this.handler)
+      this.el.removeEventListener('mouseup', this.handler)
     }
-  }
-
-  $preview.on('click', '> div > .cell[data-row][data-col]', function () {
-    var $this = $(this)
-
-    var row = $this.data('row')
-      , col = $this.data('col')
-
-    var rows = $editor.val().split('\n').slice(0, row + 1)
-      , cols = rows.pop().split('\\').slice(0, col + 1)
-
-    var above = rows.join('\n').length
-    above = rows.length ? above + 1 : 0
-
-    var selection = cols.pop()
-
-    var divider = selection.indexOf('~~')
-    if (divider >= 0) {
-      selection = selection.substring(0, divider)
-    }
-
-    var col = cols.join('\\').length
-    col = cols.length ? col + 1 : 0
-
-    var selectionStart = above + col
-      , selectionEnd = selectionStart + selection.length
-
-    $editor
-      .prop('selectionStart', selectionStart)
-      .prop('selectionEnd', selectionEnd)
-      .focus()
-
-    $preview.find('.focus').removeClass('focus')
-    $this.addClass('focus')
   })
 
-  $preview.on('click', '> div > .text[data-row]', function () {
-    var $this = $(this)
+  window.map = new Vue({
+    el: '#bme',
+    data: {
+      source: localStorage.getItem('content'),
+      selectionStart: 0,
+      selectionEnd: 0
+    },
+    watch: {
+      source: function (value) {
+        localStorage.setItem('content', value)
+      },
+      selectionStart: function (selection) {
+        // TODO: 这部分代码十分傻逼，等我学完 Vue 之后再重构
 
-    var row = $this.data('row')
+        var rows = this.source.substring(0, selection).split('\n')
+          , cols = rows.pop().split('\\')
+          , cell = cols.pop()
+          , text = cell + this.source.substring(selection).split('\n').shift()
 
-    var rows = $editor.val().split('\n').slice(0, row + 1)
-      , line = rows.pop()
+        var row = rows.length
+          , col = cols.length
 
-    var above = rows.join('\n').length
-    above = rows.length ? above + 1 : 0
+        $('.preview').find('.focus').removeClass('focus')
 
-    var divider = line.indexOf('~~')
+        var divider = text.indexOf('~~')
+          , leading = cell.length
 
-    var selectionStart = above
-      , selectionEnd = selectionStart + line.length
+        if ((divider == 0 && leading >= 1) || (divider > 0 && leading > divider)) {
+          $('.preview').find('> div[data-row="' + row + '"] > .text').addClass('focus')
+        } else {
+          $('.preview').find('> div[data-row="' + row + '"] > .cell[data-col="' + col + '"]').addClass('focus')
+        }
+      }
+    },
+    computed: {
+      width: function () {
+        var maxLength = 0
+        this.source.split('\n').forEach(function (row, i) {
+          maxLength = Math.max(maxLength, row.split('\\').length)
+        })
+        return Math.min(Math.max(maxLength * 20 + 80 + 200, 200), 600)
+      }
+    },
+    components: {
+      'preview': {
+        name: 'preview',
+        props: [ 'source' ],
+        template: '#preview-template',
+        computed: {
+          rows: function () {
+            return this.source.split('\n')
+          }
+        }
+      },
+      'row': {
+        name: 'row',
+        props: [ 'source' ],
+        template: '#row-template',
+        computed: {
+          row: function () {
+            var divider = this.source.indexOf('~~')
+            if (divider > 0) {
+              return {
+                cols: this.source.substring(0, divider).split('\\'),
+                text: this.source.substring(divider).trim()
+              }
+            } else {
+              return {
+                cols: this.source.split('\\')
+              }
+            }
+          }
+        },
+        methods: {
+          select: function (e) {
+            // TODO: 这部分代码十分傻逼，等我学完 Vue 之后再重构
 
-    if (divider > 0) {
-      selectionStart += divider
-    } else {
-      selectionStart += line.length
+            var row = $(this.$el).data('row')
+
+            var rows = this.$root.source.split('\n').slice(0, row + 1)
+              , line = rows.pop()
+
+            var above = rows.join('\n').length
+            above = rows.length ? above + 1 : 0
+
+            var divider = line.lastIndexOf('~~')
+
+            var selectionStart = above
+              , selectionEnd = selectionStart + line.length
+
+            if (divider > 0) {
+              selectionStart += divider + 2
+            } else {
+              selectionStart += line.length
+            }
+
+            this.$root.selectionStart = selectionStart
+            this.$root.selectionEnd = selectionEnd
+            $('.editor').focus()
+          }
+        }
+      },
+      'col': {
+        name: 'col',
+        props: [ 'source' ],
+        template: '#col-template',
+        computed: {
+          col: function () {
+            return {
+              icons: this.source.trim()
+                ? this.source.split('!~').map(function (icon) {
+                    return icon.trim()
+                  })
+                : []
+            }
+          }
+        },
+        methods: {
+          select: function () {
+            // TODO: 这部分代码十分傻逼，等我学完 Vue 之后再重构
+
+            var row = $(this.$el).parent('[data-row]').data('row')
+              , col = $(this.$el).data('col')
+
+            var rows = this.$root.source.split('\n').slice(0, row + 1)
+              , cols = rows.pop().split('\\').slice(0, col + 1)
+
+            var above = rows.join('\n').length
+            above = rows.length ? above + 1 : 0
+
+            var selection = cols.pop()
+
+            var divider = selection.indexOf('~~')
+            if (divider >= 0) {
+              selection = selection.substring(0, divider)
+            }
+
+            var col = cols.join('\\').length
+            col = cols.length ? col + 1 : 0
+
+            var selectionStart = above + col
+              , selectionEnd = selectionStart + selection.length
+
+            this.$root.selectionStart = selectionStart
+            this.$root.selectionEnd = selectionEnd
+            $('.editor').focus()
+          }
+        }
+      },
+      'icon': {
+        name: 'icon',
+        props: [ 'name' ],
+        template: '#icon-template',
+        computed: {
+          file: function () {
+            return 'BSicon_' + this.name.replace(/ /, '_') + '.svg'
+          },
+          hash: function () {
+            return CryptoJS.MD5(this.file).toString(CryptoJS.enc.Hex)
+          },
+          url: function () {
+            return 'https://upload.wikimedia.org/wikipedia/commons/thumb/'
+                  + this.hash.substring(0, 1) + '/'
+                  + this.hash.substring(0, 2) + '/'
+                  + encodeURIComponent(this.file) + '/'
+                  + '40px-' + encodeURIComponent(this.file) + '.png'
+          }
+        }
+      }
     }
-
-    $editor
-      .prop('selectionStart', selectionStart)
-      .prop('selectionEnd', selectionEnd)
-      .focus()
-
-    $preview.find('.focus').removeClass('focus')
-    $this.addClass('focus')
   })
 
-  $editor.on('keyup', $.debounce(250, function () {
-    localStorage.setItem('content', $editor.val())
-    render()
-    syncFocus()
-  }))
-
-  $editor.on('mouseup', function () {
-    syncFocus()
-  })
-
-  $editor.val(localStorage.getItem('content'))
-  render()
 })
