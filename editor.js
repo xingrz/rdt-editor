f0fc49972f18b098a05ac2b9a358cc0cd2173d20
XiNGRZ <chenxingyu92@gmail.com>
2016-04-24 15:56:02 +0800

    side text

--- editor.js
+++ editor.js
@@ -54,22 +54,31 @@ Vue.component('editor', {
       // TODO: 这部分代码十分傻逼，等我学完 Vue 之后再重构
 
       var rows = this.source.substring(0, selection).split('\n')
-        , cols = rows.pop().split('\\')
-        , cell = cols.pop()
+
+      var line = rows.pop()
+
+      var cols = line.split('\\')
+        , texts = line.split('~~')
+
+      var cell = cols.pop()
         , text = cell + this.source.substring(selection).split('\n').shift()
 
       var row = rows.length
         , col = cols.length
+        , index
+
+      if (text.split('~~').length == 2 && texts.length == 2) {
+        index = 2
+      } else {
+        index = texts.length - 1
+      }
 
       $('.preview').find('.focus').removeClass('focus')
 
-      var divider = text.indexOf('~~')
-        , leading = cell.length
-
-      if ((divider == 0 && leading >= 1) || (divider > 0 && leading > divider)) {
-        $('.preview').find('> div[data-row="' + row + '"] .text').addClass('focus')
+      if (index > 0) {
+        $('.preview').find('> div[data-row="' + row + '"] > .texts > div[data-index="' + index + '"]').addClass('focus')
       } else {
-        $('.preview').find('> div[data-row="' + row + '"] .cell[data-col="' + col + '"]').addClass('focus')
+        $('.preview').find('> div[data-row="' + row + '"] > .cells > div[data-col="' + col + '"]').addClass('focus')
       }
     }
   }
@@ -92,45 +101,31 @@ Vue.component('row', {
   template: '#row-template',
   computed: {
     row: function () {
-      var divider = this.source.indexOf('~~')
-      if (divider > 0) {
-        return {
-          cols: this.source.substring(0, divider).split('\\'),
-          text: this.source.substring(divider).trim()
-        }
+      var texts = this.source.split('~~')
+        , cols = texts.shift().split('\\')
+
+      if (texts.length == 1) {
+        texts = texts.map(function (text) {
+          return {
+            text: text.trim(),
+            index: 2
+          }
+        })
       } else {
-        return {
-          cols: this.source.split('\\')
-        }
-      }
-    }
-  },
-  methods: {
-    select: function (e) {
-      // TODO: 这部分代码十分傻逼，等我学完 Vue 之后再重构
-
-      var row = $(this.$el).data('row')
-
-      var rows = this.$root.$children[0].source.split('\n').slice(0, row + 1)
-        , line = rows.pop()
-
-      var above = rows.join('\n').length
-      above = rows.length ? above + 1 : 0
-
-      var divider = line.lastIndexOf('~~')
-
-      var selectionStart = above
-        , selectionEnd = selectionStart + line.length
-
-      if (divider > 0) {
-        selectionStart += divider + 2
-      } else {
-        selectionStart += line.length
+        texts = texts.map(function (text, index) {
+          return {
+            text: text.trim(),
+            index: index + 1
+          }
+        }).filter(function (text) {
+          return !!text.text
+        })
       }
 
-      this.$root.$children[0].selectionStart = selectionStart
-      this.$root.$children[0].selectionEnd = selectionEnd
-      $('.editor').focus()
+      return {
+        cols: cols,
+        texts: texts
+      }
     }
   }
 })
@@ -170,10 +165,10 @@ Vue.component('col', {
         selection = selection.substring(0, divider)
       }
 
-      var col = cols.join('\\').length
-      col = cols.length ? col + 1 : 0
+      var start = cols.join('\\').length
+      start = cols.length ? start + 1 : 0
 
-      var selectionStart = above + col
+      var selectionStart = above + start
         , selectionEnd = selectionStart + selection.length
 
       this.$root.$children[0].selectionStart = selectionStart
@@ -200,6 +195,43 @@ Vue.component('icon', {
             + this.hash.substring(0, 2) + '/'
             + encodeURIComponent(this.file) + '/'
             + '40px-' + encodeURIComponent(this.file) + '.png'
+    }
+  }
+})
+
+Vue.component('text', {
+  name: 'text',
+  props: [ 'text', 'index' ],
+  template: '#text-template',
+  methods: {
+    select: function (e) {
+      // TODO: 这部分代码十分傻逼，等我学完 Vue 之后再重构
+
+      var row = $(this.$el).parents('[data-row]').data('row')
+        , index = $(this.$el).data('index')
+
+      console.log(index)
+
+      var rows = this.$root.source.split('\n').slice(0, row + 1)
+        , texts = rows.pop().split('~~')
+
+      if (texts.length > 2) {
+        texts = texts.slice(0, index + 1)
+      }
+
+      var above = rows.join('\n').length
+      above = rows.length ? above + 1 : 0
+
+      var selection = texts.pop()
+
+      var start = texts.join('~~').length + 2
+
+      var selectionStart = above + start
+        , selectionEnd = selectionStart + selection.length
+
+      this.$root.$children[0].selectionStart = selectionStart
+      this.$root.$children[0].selectionEnd = selectionEnd
+      $('.editor').focus()
     }
   }
 })
