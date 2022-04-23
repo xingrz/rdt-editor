cb9b2488774dc453ff665794cffe11e5aa9595af
XiNGRZ <hi@xingrz.me>
2022-04-23 17:56:36 +0800

    Migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    vue(),
+  ],
+});
