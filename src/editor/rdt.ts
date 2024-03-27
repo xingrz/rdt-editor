1007ccce6615afecd1af290617eb7169076059f9
XiNGRZ <hi@xingrz.me>
2024-03-27 17:00:47 +0800

    Rework highlight rules for RDT syntax

--- src/editor/rdt.ts
+++ src/editor/rdt.ts
@@ -1,5 +1,6 @@
 import { Mode as TextMode } from 'ace-code/src/mode/text';
 import { TextHighlightRules } from 'ace-code/src/mode/text_highlight_rules';
+import type { Ace } from 'ace-code';
 
 export class RDTHighlightRules extends TextHighlightRules {
   constructor() {
@@ -7,20 +8,77 @@ export class RDTHighlightRules extends TextHighlightRules {
     this.normalizeRules();
   }
 
+  _basicRules: Ace.HighlightRule[] = [
+    {
+      token: 'text',
+      regex: /\\/,
+      next: 'start',
+    },
+    {
+      token: 'keyword.operator',
+      regex: /!@/,
+      next: 'cellLink',
+    },
+    {
+      token: 'keyword.operator',
+      regex: /!~/,
+      next: 'start',
+    },
+    {
+      token: 'keyword.operator',
+      regex: /(!_)|(__)/,
+      next: 'cellParams',
+    },
+    {
+      token: 'text',
+      regex: /~~/,
+      next: 'sideText',
+    },
+    {
+      token: 'comment.block',
+      regex: /<!--/,
+      next: 'comment',
+    },
+  ];
+
   $rules = {
     start: [
+      ...this._basicRules,
       {
-        token: 'keyword.operator.rdt',
-        regex: '(?:\\\\|!~)',
+        defaultToken: 'keyword.control',
+      },
+    ],
+    cellLink: [
+      ...this._basicRules,
+      { defaultToken: 'constant.character' },
+    ],
+    cellParams: [
+      ...this._basicRules,
+      { defaultToken: 'string' },
+    ],
+    sideText: [
+      {
+        token: 'text',
+        regex: /~~/,
       },
       {
-        token: 'punctuation.definition.string',
-        regex: '(?:~~.*$)',
+        token: 'comment.block',
+        regex: /<!--/,
+        next: 'comment',
       },
       {
-        token: 'keyword.control.statement.rdt',
-        regex: '(?:[^\\\\(~~)(!~)]+)',
+        regex: /$/,
+        next: 'start',
       },
+      { defaultToken: 'string' },
+    ],
+    comment: [
+      {
+        token: 'comment.block',
+        regex: /-->/,
+        next: 'start',
+      },
+      { defaultToken: 'comment.block' },
     ],
   };
 }
